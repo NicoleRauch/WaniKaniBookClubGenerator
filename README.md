d30dcf4f1ffe181f7384fb3f8901a7b869aafefa
Nicole Rauch <info@nicole-rauch.de>
2019-10-02 10:19:46 +0200

    Only one config file for all weeks of a given book, where only the weekly information is duplicated. Thanks to Myria for the suggestion

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ You need to install node.js (at least version 8) [which can be downloaded here](
 
 In order to get the result you want, you need to create a configuration file (see the provided example file `kitchen_week1.json`).
 
-It contains the following parameters:
+It contains the following variables:
 
 |Name|Type|Explanation|
 |----|----|-----------|
@@ -20,20 +20,26 @@ It contains the following parameters:
 |`bookName`|String|The name of the book that this post is for|
 |`bookImage`|String|The full image descriptor (you can usually steal this from the book's home thread).|
 |`bookHomeThreadURL`|String|The URL of the book's home thread (or the last part of the URL)|
-|`week`|Number|The number of the week that this post is for|
-|`weekStartDate`|String|When does this part's reading start, in human-readable format (e.g. "June 3rd")|
-|`bookPreviousWeekURL`|String; optional|The URL of the previous week's thread (if this is not applicable, it can be left out or left empty).|
-|`bookNextWeekURL`|String; optional|The URL of the next week's thread (if this is not yet known, it can be left out or left empty).|
 |`numberOfTheLastWeek`|Number|The number of the last week for this book (so that the last week does not get a "next week" link).|
-|`readingEndPage`|Number|The end page of this week's reading, in the paper book.| 
-|`readingEndPercent`|Number|The percentage of the last sentence of this week's reading.| 
-|`readingEndPhrase`|String|The last phrase of this week's reading.| 
-|`readingPages`|Number|The number of pages that this week's reading covers.| 
 |`isOnFloFlo`|Boolean|Set this to `true` if you want a FloFlo section to be generated, `false` if not.|
 |`hasReadAlongSession`|Boolean|Set this to `true` if you want information about a Read-Along session to appear, `false` if not.|
 |`readAlongWeekday`|String; optional|The weekday of the read-along session. Only required when the read-along session should be shown.|
 |`readAlongJSTHuman`|String; optional|The time of the read-along session in JST, in human-readable format (e.g. `9:30pm`). Only required when the read-along session should be shown.|
 |`readAlongJSTComputer`|String; optional|The time of the read-along session in JST, in computer-readable format (e.g. `21:30:00`). Only required when the read-along session should be shown.|
+|`weeks`|Array|The list of the configurations for each week (see below).|
+
+The configurations for each week contain the following variables:
+
+|Name|Type|Explanation|
+|----|----|-----------|
+|`week`|Number|The number of the week that this post is for|
+|`weekStartDate`|String|When does this part's reading start, in human-readable format (e.g. "June 3rd")|
+|`bookPreviousWeekURL`|String; optional|The URL of the previous week's thread (if this is not applicable, it can be left out or left empty).|
+|`bookNextWeekURL`|String; optional|The URL of the next week's thread (if this is not yet known, it can be left out or left empty).|
+|`readingEndPage`|Number|The end page of this week's reading, in the paper book.| 
+|`readingEndPercent`|Number|The percentage of the last sentence of this week's reading.| 
+|`readingEndPhrase`|String|The last phrase of this week's reading.| 
+|`readingPages`|Number|The number of pages that this week's reading covers.| 
 |`readAlongNextDate`|String; optional|The date of the next read-along session, in computer-readable format (e.g. `2019-03-15`). Only required when the read-along session should be shown.|
 
 
@@ -83,9 +89,10 @@ In order to run the generator, type
 
 `node generator <config-file.json>`
 
-in the current directory. It will read the config file and produce a markdown file
-with the same name, but with the ending `.md`, e.g. for the config file `config-file.json` you will get `config-file.md`.
-You can then copy and paste this config file into WaniKani's forum.
+in the current directory. It will read the config file and produce a markdown file for each week.
+The markdown files will have the same name, but with the ending `_weekN.md`, e.g. for the config file `config-file.json` 
+you will get `config-file_week1.md` to `config-file_week123.md` (one for each entry in the `weeks` array).
+You can then copy and paste the resulting `.md` files into WaniKani's forum.
 
 If you want to update the forum post at a later stage (e.g. because you now know the next week's URL),
 simply update the configuration file with the new information, run the generator again and update the
--- generator.js
+++ generator.js
@@ -2,68 +2,74 @@ const fs = require('fs');
 const path = require('path');
 
 const urlSnippetOf = (url) => {
-    if(!url) { return ""; } // safety measurement: should be caught in template already
+    if (!url) {
+        return "";
+    } // safety measurement: should be caught in template already
     return "https://community.wanikani.com/t/x/" + url.substring(url.lastIndexOf('/') + 1);
 };
 
 const configFileName = process.argv[2];
 const config = JSON.parse(fs.readFileSync("./" + configFileName, {encoding: "utf8"}));
 
-var template = fs.readFileSync("./" + config.template, {encoding: "utf8"});
 
-// Replacements:
-// configured values:
-template = template.replace(/\$bookClubName\$/g, config.bookClubName);
-template = template.replace(/\$bookClubURL\$/g, urlSnippetOf(config.bookClubURL));
-template = template.replace(/\$bookName\$/g, config.bookName);
-template = template.replace(/\$bookImage\$/g, config.bookImage);
-template = template.replace(/\$bookHomeThreadURL\$/g, urlSnippetOf(config.bookHomeThreadURL));
-template = template.replace(/\$week\$/g, config.week);
-template = template.replace(/\$weekStartDate\$/g, config.weekStartDate);
-template = template.replace(/\$bookPreviousWeekURL\$/g, urlSnippetOf(config.bookPreviousWeekURL));
-template = template.replace(/\$bookNextWeekURL\$/g, urlSnippetOf(config.bokNextWeekURL));
-template = template.replace(/\$numberOfTheLastWeek\$/g, config.numberOfTheLastWeek);
-template = template.replace(/\$readingEndPage\$/g, config.readingEndPage);
-template = template.replace(/\$readingEndPercent\$/g, config.readingEndPercent);
-template = template.replace(/\$readingEndPhrase\$/g, config.readingEndPhrase);
-template = template.replace(/\$readingPages\$/g, config.readingPages);
-template = template.replace(/\$isOnFloFlo\$/g, config.isOnFloFlo);
-template = template.replace(/\$hasReadAlongSession\$/g, config.hasReadAlongSession);
-template = template.replace(/\$readAlongWeekday\$/g, config.readAlongWeekday);
-template = template.replace(/\$readAlongJSTHuman\$/g, config.readAlongJSTHuman);
-template = template.replace(/\$readAlongJSTComputer\$/g, config.readAlongJSTComputer); // TODO
-template = template.replace(/\$readAlongNextDate\$/g, config.readAlongNextDate);
-// generated values:
-template = template.replace(/\$previousWeek\$/g, config.week - 1);
-template = template.replace(/\$nextWeek\$/g, config.week + 1);
+config.weeks.map(weekConfig => {
 
-// conditionals:
-const reducer = (acc, current) => {
-    if(current.startsWith("::if")){
-        const condition = eval(current.substring(4));
-        return {...acc, removeLines: !condition}; // ignore ::if line, set whether to remove lines or not
-    }
-    if(current.startsWith("::else")){
-        return {...acc, removeLines: !acc.removeLines}; // ignore ::else line, invert the removeLines condition
-    }
-    if(current.startsWith("::endif")){
-        return {...acc, removeLines: false}; // ignore ::endif line, reset back to normal
-    }
-    if(acc.removeLines) {
-        return acc; // ignore current line
-    }
-    return {...acc, resultLines: acc.resultLines.concat(current)}; // add current line
-};
+    // reload the template for each week!
+    var template = fs.readFileSync("./" + config.template, {encoding: "utf8"});
+    // Replacements:
+    // configured values:
+    template = template.replace(/\$bookClubName\$/g, config.bookClubName);
+    template = template.replace(/\$bookClubURL\$/g, urlSnippetOf(config.bookClubURL));
+    template = template.replace(/\$bookName\$/g, config.bookName);
+    template = template.replace(/\$bookImage\$/g, config.bookImage);
+    template = template.replace(/\$bookHomeThreadURL\$/g, urlSnippetOf(config.bookHomeThreadURL));
+    template = template.replace(/\$numberOfTheLastWeek\$/g, config.numberOfTheLastWeek);
+    template = template.replace(/\$isOnFloFlo\$/g, config.isOnFloFlo);
+    template = template.replace(/\$hasReadAlongSession\$/g, config.hasReadAlongSession);
+    template = template.replace(/\$readAlongWeekday\$/g, config.readAlongWeekday);
+    template = template.replace(/\$readAlongJSTHuman\$/g, config.readAlongJSTHuman);
+    template = template.replace(/\$readAlongJSTComputer\$/g, config.readAlongJSTComputer); // TODO
 
-const templateLines = template.split("\n");
-const result = templateLines.reduce(
-    reducer,
-    {
-        removeLines: false,
-        resultLines: []
-    }
-);
+    template = template.replace(/\$week\$/g, weekConfig.week);
+    template = template.replace(/\$weekStartDate\$/g, weekConfig.weekStartDate);
+    template = template.replace(/\$bookPreviousWeekURL\$/g, urlSnippetOf(weekConfig.bookPreviousWeekURL));
+    template = template.replace(/\$bookNextWeekURL\$/g, urlSnippetOf(weekConfig.bokNextWeekURL));
+    template = template.replace(/\$readingEndPage\$/g, weekConfig.readingEndPage);
+    template = template.replace(/\$readingEndPercent\$/g, weekConfig.readingEndPercent);
+    template = template.replace(/\$readingEndPhrase\$/g, weekConfig.readingEndPhrase);
+    template = template.replace(/\$readingPages\$/g, weekConfig.readingPages);
+    template = template.replace(/\$readAlongNextDate\$/g, weekConfig.readAlongNextDate);
+    // generated values:
+    template = template.replace(/\$previousWeek\$/g, weekConfig.week - 1);
+    template = template.replace(/\$nextWeek\$/g, weekConfig.week + 1);
 
-const resultFileName = configFileName.replace(".json", ".md");
-fs.writeFileSync("./" + resultFileName, result.resultLines.join("\n"), {encoding: "utf8"});
+    // conditionals:
+    const reducer = (acc, current) => {
+        if (current.startsWith("::if")) {
+            const condition = eval(current.substring(4));
+            return {...acc, removeLines: !condition}; // ignore ::if line, set whether to remove lines or not
+        }
+        if (current.startsWith("::else")) {
+            return {...acc, removeLines: !acc.removeLines}; // ignore ::else line, invert the removeLines condition
+        }
+        if (current.startsWith("::endif")) {
+            return {...acc, removeLines: false}; // ignore ::endif line, reset back to normal
+        }
+        if (acc.removeLines) {
+            return acc; // ignore current line
+        }
+        return {...acc, resultLines: acc.resultLines.concat(current)}; // add current line
+    };
 
+    const templateLines = template.split("\n");
+    const result = templateLines.reduce(
+        reducer,
+        {
+            removeLines: false,
+            resultLines: []
+        }
+    );
+
+    const resultFileName = configFileName.replace(".json", "_week" + weekConfig.week + ".md");
+    fs.writeFileSync("./" + resultFileName, result.resultLines.join("\n"), {encoding: "utf8"});
+});
